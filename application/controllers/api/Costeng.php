78e4b55e4527f401fdd26711183316b7cd4cae73
Hanifka <81150342+Hanifka@users.noreply.github.com>
2022-05-31 12:36:08 +0700

    post get put

--- application/controllers/api/Costeng.php
+++ application/controllers/api/Costeng.php
@@ -19,8 +19,14 @@ class costeng extends REST_Controller {
     }
 
     public function index_get()
-    {
-        $data = $this->costeng->getCosteng('biaya_reparasi');
+    {   
+        $id = $this->get('id');
+        if($id === null){
+            $data = $this->costeng->getCosteng('biaya_reparasi');
+        }else{
+            $data = $this->costeng->getCosteng('biaya_reparasi',$id);
+        }
+        
 
         if ($data){
             $this->set_response([
@@ -28,7 +34,89 @@ class costeng extends REST_Controller {
                 'data' => $data
             ], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
 
+        }else{
+            $this->set_response([
+                'status' => false,
+                'message' => 'id tidak ada'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
         }
         
     }
+    public function index_delete(){
+        // $data = $this->costeng->getCosteng('biaya_reparasi');
+        $id = $this->delete('id');
+        if( $id === null){
+            $this->set_response([
+                'status' => false,
+                'message' => 'ISI ID !!'
+            ], REST_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404) being the HTTP response code
+        }else{
+            // $data = $this->costeng->deleteCosteng('biaya_reparasi');
+            if( $this->costeng->deleteCosteng('biaya_reparasi',$id) > 0){
+                $this->set_response([
+                    'status' => true,
+                    'data' => $id,
+                    'message' => 'deleted'
+                ], REST_Controller::HTTP_NO_CONTENT); // NOT_FOUND (404) being the HTTP response code
+            }else{
+                $this->set_response([
+                    'status' => false,
+                    'message' => 'id tidak ada'
+                ], REST_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404) being the HTTP response code
+            }
+        } 
+    }
+
+    public function index_post(){
+        $isi = [
+            'mesin'=> $this->post('mesin'),
+            'biaya'=> $this->post('biaya'),
+            'Engineer'=> $this->post('Engineer'),
+            'status'=> $this->post('status'),
+            'keterangan'=> $this->post('keterangan'),
+            'ID_budgeting'=> $this->post('ID_budgeting')
+
+
+        ];
+        if ($this->costeng->createCosteng('biaya_reparasi',$isi)> 0){
+            $this->set_response([
+                'status' => true,
+                'message' => 'new data created'
+            ], REST_Controller::HTTP_CREATED); // NOT_FOUND (404) being the HTTP response code
+        }else{
+            $this->set_response([
+                'status' => false,
+                'message' => 'id tidak ada'
+            ], REST_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404) being the HTTP response code
+        }
+
+        
+    }
+
+    public function index_put(){
+        $id = $this->put('id');
+        $isi = [
+            'mesin'=> $this->put('mesin'),
+            'biaya'=> $this->put('biaya'),
+            'Engineer'=> $this->put('Engineer'),
+            'status'=> $this->put('status'),
+            'keterangan'=> $this->put('keterangan'),
+            'ID_budgeting'=> $this->post('ID_budgeting')
+        ];
+        if ($this->costeng->updateCosteng('biaya_reparasi',$isi,$id)> 0){
+            $this->set_response([
+                'status' => true,
+                'message' => 'new data updated'
+            ], REST_Controller::HTTP_NO_CONTENT); // NOT_FOUND (404) being the HTTP response code
+        }else{
+            $this->set_response([
+                'status' => false,
+                'message' => 'gagal update'
+            ], REST_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404) being the HTTP response code
+        }
+
+    }
+
+
+
 }
--- application/models/Costeng_model.php
+++ application/models/Costeng_model.php
@@ -3,7 +3,26 @@
 
 class Costeng_model extends CI_Model
 {
-    public function getCosteng($data){
-        return $this->db->get($data)->result_array();
+    public function getCosteng($data,$id = null){
+        if( $id === null ){
+            return $this->db->get($data)->result_array();
+        }else{
+            return $this->db->get_where($data,['id'=> $id])->result_array();
+        }
+        // return $this->db->get($data)->result_array();
+    }
+
+    public function deleteCosteng($data,$id){
+        $this->db->delete($data,['id'=> $id]);
+        return $this->db->affected_rows();
+
+    }
+    public function createCosteng($data,$isi){
+        $this->db->insert($data,$isi);
+        return $this->db->affected_rows();
+    }
+    public function updateCosteng($data,$isi,$id){
+        $this->db->update($data,$isi,['id'=>$id]);
+        return $this->db->affected_rows();
     }
 }
